ff7be7910a8fbab55f73d1c8015a60860125e677
Oriiromero <oriana.nahir.r@gmail.com>
2023-05-22 17:45:17 +0200

    fixing home and adding back arrow

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,16 +1,17 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./product.scss";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ProductsContext } from "../Shared/ProductsContext";
 
 const Product = () => {
   let { id } = useParams();
-  const { allProducts, cartProducts, setCartProducts } = useContext(ProductsContext);
+  const { allProducts, cartProducts, setCartProducts } =
+    useContext(ProductsContext);
   const [singleProduct, setSingleProduct] = useState([null]);
   const [productStars, setProductStars] = useState();
   const [productsCount, setProductCount] = useState();
-  const [selectedValue, setSelectedValue] = useState('');
-  
+  const [selectedValue, setSelectedValue] = useState("");
+  const navigate = useNavigate();
 
   //Check if the product we need matches with the id in the url
   useEffect(() => {
@@ -20,7 +21,6 @@ const Product = () => {
         setSingleProduct(product);
         setProductStars(product.rating.rate);
         setProductCount(product.rating.count);
-
       }
     });
   }, [id]);
@@ -28,29 +28,29 @@ const Product = () => {
   //Calculate the background to print into the stars
   let pixelPercentage = (100 / 5) * productStars;
 
-  //Save the amount selected 
+  //Save the amount selected
   const handleSelectChange = (event) => {
     setSelectedValue(event.target.value);
   };
 
-  //adding product to the cart 
+  //adding product to the cart
   const addToCart = () => {
-
     let addedProduct = {
       title: singleProduct.title,
       price: singleProduct.price,
-      amount: selectedValue
+      amount: selectedValue,
     };
 
     setCartProducts([...cartProducts, addedProduct]);
 
     console.log(addedProduct);
-  }
+  };
 
   console.log(cartProducts);
 
   return (
     <div className="product-page">
+      <img onClick={()=> navigate('/products')} className='product-page_back' src="/assets/left-arrow.png" alt="back-arrow" />
       <div className="main-product">
         <div className="main-product_img">
           <img src={singleProduct.image} alt={singleProduct.id} />
@@ -90,7 +90,10 @@ const Product = () => {
             <p className="random"> ({productsCount}) </p>
           </div>
           <p className="main-product_info-price">€ {singleProduct.price}</p>
-          <select className="main-product_info-amount" onChange={handleSelectChange}>
+          <select
+            className="main-product_info-amount"
+            onChange={handleSelectChange}
+          >
             <option className="main-product_info-amount_opt" value="1">
               1
             </option>
@@ -104,7 +107,9 @@ const Product = () => {
               4
             </option>
           </select>
-          <button onClick={addToCart} className="main-product_info-cart">Add to cart</button>
+          <button onClick={addToCart} className="main-product_info-cart">
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
